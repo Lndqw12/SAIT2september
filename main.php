4ba61c0a037efa36cd0376e9981912d7584d776d
Lndqw12 <COMP222012016@mail.ru>
2016-09-02 17:21:24 +0300

    2september

---
+++ main.php
@@ -0,0 +1,15 @@
+﻿<meta charset ="utf-8">
+<?php
+echo "<pre>";
+$name =$_REQUEST['user'];
+echo "Привет,"." ".$name;
+echo "</br>";
+echo "Вот данные о комьпютере";
+echo "</br>";
+echo $_SERVER['REMOTE_ADDR'];
+echo "</br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "</br>";
+print_r($GLOBALS);
+echo "</pre>";
+?>
